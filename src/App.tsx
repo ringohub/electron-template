c7132e732aa0ecf8d9df264cb20d9399a6ff83db
aoki <ringohub@gmail.com>
2020-06-12 21:38:03 +0900

    Logo update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import elogo from "./electron-logo.svg";
+import reactLogo from "./react-logo.svg";
+import electronLogo from "./electron-logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div></div>
         <p>
-          Hello
-          <img src={logo} className="App-logo" alt="logo" />
-          React with{" "}
-          <img src={elogo} className="App-logo" alt="electron-logo" /> Electron.
+          <img src={reactLogo} className="App-logo" alt="logo" />
+          Hello React with Electron
+          <img src={electronLogo} className="App-logo" alt="electron-logo" />
         </p>
       </header>
-      \
     </div>
   );
 }
